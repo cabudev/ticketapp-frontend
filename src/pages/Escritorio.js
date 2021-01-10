6d8907e7a59269b333be51eacba001115ab8f300
Catalin <budaimc@gmail.com>
2021-01-10 11:00:49 +0100

    Frontend Finalizado

--- src/pages/Escritorio.js
+++ src/pages/Escritorio.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { Redirect, useHistory } from 'react-router-dom';
 import { Row, Col, Typography, Button, Divider } from 'antd';
 import { CloseCircleOutlined, RightOutlined } from '@ant-design/icons';
+
 import { useHideMenu } from '../hooks/useHideMenu';
 import { getUsuarioStorage } from '../helpers/getUsuarioStorage';
-import { Redirect, useHistory } from 'react-router-dom';
+import { SocketContext } from '../context/SocketContext';
 
 const { Title, Text } = Typography;
 
@@ -11,6 +13,8 @@ export const Escritorio = () => {
 
     useHideMenu(false);
     const [ usuario ] = useState( getUsuarioStorage() );
+    const { socket } = useContext(SocketContext);
+    const [ticket, setTicket] = useState(null);
     const history = useHistory();
 
     const salir = () => {
@@ -19,7 +23,9 @@ export const Escritorio = () => {
     }
 
     const siguienteTicket = () => {
-        console.log('siguienteTicket');
+        socket.emit('siguiente-ticket-escritorio', usuario, ( ticket ) => {
+            setTicket(ticket);
+        });
     }
 
     if ( !usuario.agente || !usuario.escritorio ) {
@@ -51,17 +57,22 @@ export const Escritorio = () => {
 
             <Divider />
 
-            <Row>
-                <Col>
-                    <Text>Está atendiendo el ticket número: </Text>
-                    <Text 
-                        style={{ fontSize: 30 }} 
-                        type="danger"
-                    > 
-                    55
-                    </Text>
-                </Col>
-            </Row>
+            {
+                ticket && (
+                    <Row>
+                        <Col>
+                            <Text>Está atendiendo el ticket número: </Text>
+                            <Text 
+                                style={{ fontSize: 30 }} 
+                                type="danger"
+                            > 
+                            { ticket.numero }
+                            </Text>
+                        </Col>
+                    </Row>
+                )
+            }
+            
 
 
             <Row>
